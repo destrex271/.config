0a41c99be67e9966d1c263aa1470ab48dcd6a975
Akshat Jaimini <destrex271@gmail.com>
2023-12-30 22:45:49 +0530

    barber added to nvim

--- nvim/lua/akshat/lazy.lua
+++ nvim/lua/akshat/lazy.lua
@@ -36,6 +36,21 @@ require("lazy").setup({
         -- event = "InsertEnter",
         opts = {} -- this is equalent to setup({}) function
     },
+    {
+        'romgrk/barbar.nvim',
+        dependencies = {
+            'lewis6991/gitsigns.nvim', -- OPTIONAL: for git status
+            'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
+        },
+        init = function() vim.g.barbar_auto_setup = false end,
+        opts = {
+            -- lazy.nvim will automatically call setup for you. put your options here, anything missing will use the default:
+            -- animation = true,
+            -- insert_at_start = true,
+            -- …etc.
+        },
+        version = '^1.0.0', -- optional: only update when a new 1.x version is released
+    },
 	{'williamboman/mason.nvim'},
 	{'williamboman/mason-lspconfig.nvim'},
 	{'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'},
